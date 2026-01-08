64d77368b68c440505c660c7270c22b245d504ef
zubairz40-z <zahirzubair740@gmail.com>
2026-01-08 12:41:46 +0600

    added

---
+++ Problem6.js
@@ -0,0 +1,12 @@
+//Problem 6
+function sumArr(arr){
+    let sum =0;
+
+    for(let num of arr){
+        sum+=num;
+    }
+
+return sum;
+}
+
+console.log(sumArr([1,2,3,4]))
---
+++ Problem7.js
@@ -0,0 +1,5 @@
+function evenNum(arr){
+
+  return arr.filter(num=>num%2 ===0)
+}
+console.log(evenNum([1,2,3,4,5,6]))
